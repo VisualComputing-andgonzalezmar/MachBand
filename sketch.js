cb91cff2901757011883abe412b375647a3f351d
Sergio Gonzalez <andgonzalezmar@unal.edu.co>
2022-04-05 00:07:29 -0500

    Se suaviza el ruido Perlin

--- sketch.js
+++ sketch.js
@@ -20,28 +20,29 @@ function setup() {
 
 function draw() {
 
-  var yoff = 0;
-  for (var y = 0; y < filas; y++) {
-    var xoff = 0;
-    for (var x = 0; x < columnas; x++) {
-      terreno[x][y] = map(noise(xoff, yoff), 0, 1, -350, 350);
-      xoff += 0.1;
-    }
-    yoff += 0.1;
-  }
-
   rotateX(PI/3);
   translate(-largoTerreno / 2, -anchoTerreno / 2);
-  for (var y = 0; y < filas - 1; y++) {
-    beginShape(TRIANGLE_STRIP);
+
+  var suavizarY = 0;
+  for (var y = 0; y < filas; y++) {
+    var suavizarX = 0;
     for (var x = 0; x < columnas; x++) {
+      terreno[x][y] = map(noise(suavizarX, suavizarY), 0, 1, -350, 350);
+      suavizarX += 0.1;
+    }
+    suavizarY += 0.1;
+  }
+
+  for (var x = 0; x < filas - 1; x++) {
+    beginShape(TRIANGLE_STRIP);
+    for (var y = 0; y < columnas; y++) {
       vertex(x * escala, y * escala, terreno[x][y]);
       if(terreno[x][y] >= 200){fill(255);}//Blanco
       if(terreno[x][y] < 200 && terreno[x][y] >= 100){fill(255,0,0);}//Rojo
       if(terreno[x][y] < 100 && terreno[x][y] >= 0){fill(255,255,0);}//Amarillo
       if(terreno[x][y] < 0 && terreno[x][y] >= -100){fill(0,255,0);}//Verde
       if(terreno[x][y] < -100){fill(0,0,250);}//Azul
-      vertex(x * escala, (y + 1) * escala, terreno[x][y + 1]);
+      vertex((x+1) * escala, y * escala, terreno[x+1][y]);
       if(terreno[x][y] >= 200){fill(255);}//Blanco
       if(terreno[x][y] < 200 && terreno[x][y] >= 100){fill(255,0,0);}//Rojo
       if(terreno[x][y] < 100 && terreno[x][y] >= 0){fill(255,255,0);}//Amarillo
